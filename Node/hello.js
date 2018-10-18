aa59b6ed7326c22c06801e932621abb92fb0d2b5
Andres <floresc.andres@intracitygeeks.org>
2018-10-17 23:31:19 -0400

    hello.js node

---
+++ Node/hello.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("Hello! lets run Dog");
+    // response.write("Dog is running");
+    // setTimeout(function(){
+    //     response.write("Dog is done.");
+        response.end();
+    // }, 5000);
+    
+}).listen(8080);
+
+console.log('listening on port 8080...');
