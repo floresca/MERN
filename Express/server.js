6ea1c4e919321ecdb432445e7b420117f8b5dcc4
Andres <floresc.andres@intracitygeeks.org>
2018-10-20 14:01:28 -0400

    express server

---
+++ Express/server.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+app.get('/', (requet, response) => response.send('<h1>hello</h1>'))
+
+app.get('/test', (req, res) => res.send('<h1>test route</h1>'))
+
+// app.get('/test/:num', (req, res) => res.send(`<h1>${req.params.num}</h1>`))
+
+app.get('/test/add/:num', (req, res) => {
+    const num = parseInt(req.params.num, 10);
+    res.send(`<h1>${5 + num}</h1>`)
+})
+
+app.get('/test/subtract', (req, res) => {
+    const num = parseInt(req.query.num, 10);
+    res.send(`<h1>${num - 5}</h1>`);
+})
+
+app.post('text/multiply', (req, res) => {
+    const num = parseInt(req.body.num, 10);
+    res.json({result: num * 5});
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App listning on port ${port}`))
