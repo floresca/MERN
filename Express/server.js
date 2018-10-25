776aaeab917fcd85314879027cf69af602aeac2e
Andres <floresc.andres@intracitygeeks.org>
2018-10-24 23:10:31 -0400

    sever issues

--- Express/server.js
+++ Express/server.js
@@ -1,14 +1,14 @@
-const express = require ('express');
-const app = express();
-const bodyParser = require('body-parser');
+// const express = require ('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json())
 
-app.get('/', (requet, response) => response.send('<h1>hello</h1>'))
+// app.get('/', (requet, response) => response.send('<h1>hello</h1>'))
 
-app.get('/:random', (req, res) => res.send(`<h1>${req.query.random}</h1>`))
+// app.get('/:random', (req, res) => res.send(`<h1>${req.query.random}</h1>`))
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`App listning on port ${port}`))
+// app.listen(port, () => console.log(`App listning on port ${port}`))
